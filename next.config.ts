1ba3a6363dad1a4ce22788c846cca97f56be051e
piyush2501pm <piyush2501pm@gmail.com>
2025-07-25 12:27:23 +0000

    firebase studio configs - 0

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  allowedDevOrigins: [
+    "3000-firebase-ai-agent-ui-1753444526765.cluster-fdkw7vjj7bgguspe3fbbc25tra.cloudworkstations.dev"
+  ]
 };
 
 export default nextConfig;
